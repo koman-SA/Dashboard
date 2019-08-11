51298e2b37caa3fffaba15a29098afc88298953c
koman-SA <52891065+koman-SA@users.noreply.github.com>
2019-08-11 20:38:37 +0200

    Submit New Dashboard

---
+++ src/routes.js
@@ -0,0 +1,108 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.7.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+import Unarchive from "@material-ui/icons/Unarchive";
+import Language from "@material-ui/icons/Language";
+import Backup from "@material-ui/icons/Backup";
+
+// core components/views for Admin layout
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import UserProfile from "views/UserProfile/UserProfile.jsx";
+import TableList from "views/TableList/TableList.jsx";
+import Typography from "views/Typography/Typography.jsx";
+import Icons from "views/Icons/Icons.jsx";
+import Maps from "views/Maps/Maps.jsx";
+import NotificationsPage from "views/Notifications/Notifications.jsx";
+import UpgradeToPro from "views/UpgradeToPro/UpgradeToPro.jsx";
+// core components/views for RTL layout
+import RTLPage from "views/RTLPage/RTLPage.jsx";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "collapse",
+    name: "Environment",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: BubbleChart,
+    component: UserProfile,
+
+    layout: "/admin"
+  },
+  {
+    path: "/table",
+    name: "Monitor",
+    rtlName: "قائمة الجدول",
+    icon: "Network",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Administration",
+    rtlName: "طباعة",
+    icon: LibraryBooks,
+    component: Typography,
+    layout: "/admin"
+  },
+  {
+    path: "/icons",
+    name: "Backup",
+    rtlName: "الرموز",
+    icon: Backup,
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/Maps",
+    name: "Secure",
+    rtlName: "خرائط",
+    icon: LocationOn,
+    component: Maps,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Advanced",
+    rtlName: "إخطارات",
+    icon: Notifications,
+    component: NotificationsPage,
+    layout: "/admin"
+  },
+  {
+    path: "/rtl-page",
+    name: "Settings",
+    rtlName: "پشتیبانی از راست به چپ",
+    icon: Language,
+    component: RTLPage,
+    layout: "/rtl"
+  }
+];
+
+export default dashboardRoutes;
